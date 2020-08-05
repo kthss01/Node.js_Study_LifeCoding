c3306c4ed00a8b5a1c6a7237ffd40b9dd482cebe
kthss01 <kthss01@naver.com>
2020-08-05 23:15:10 +0900

    5일차 JavaScript - Boolean, 비교 연산자, 제어문, 조건문 Node.js - 콘솔에서의 입력값 App - Not found 오류 구현 App - 홈페이지 구현

--- nodejs/main.js
+++ nodejs/main.js
@@ -5,40 +5,79 @@ var url = require('url'); // fs
 var app = http.createServer(function(request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
-  var title = queryData.id;
+  var pathname = url.parse(_url, true).pathname;
+
   // console.log(queryData.id);
-  if (_url == '/') {
-    // url = '/index.html';
-    title = "Welcome";
-  }
-  if (_url == '/favicon.ico') {
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
+
+  // if (_url == '/') {
+  //   // url = '/index.html';
+  //   title = "Welcome";
+  // }
+  // if (_url == '/favicon.ico') {
+  //   return response.writeHead(404);
+  // }
+
+  // response.writeHead(200);
   // console.log(__dirname + url);
 
-  fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
-    var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-    `;
-    response.end(template);
-  });
+  // console.log(url.parse(_url, true));
+  if (pathname === '/') {
+    if (queryData.id === undefined) {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+        response.writeHead(200); // 웹서버가 응답할 때
+        response.end(template);
+      });
+    } else {
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+        var title = queryData.id;
+        var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+        response.writeHead(200); // 웹서버가 응답할 때
+        response.end(template);
+      });
+    }
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
 
   // response.end(fs.readFileSync(__dirname + url)); // 파일을 읽어주는거
   // response.end(queryData.id);
