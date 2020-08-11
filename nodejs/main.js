3312282174d4f5b6d3423f75de0a0736521647b8
kthss01 <kthss01@naver.com>
2020-08-11 09:57:14 +0900

    7일차 Node.js - 동기와 비동기 그리고 콜백, 패키지 매니저와 PM2 HTML - Form App - 함수를 이용해서 정리 정돈하기 (이전에 안함), 글 생성 UI 만들기, POST 방식으로 전송된 데이터 받기, 파일 생성과 리다이렉션

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,6 +1,37 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url'); // fs
+var qs = require('querystring');
+
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    <a href="/create">create</a>
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList(filelist) {
+  var list = '<ul>';
+  var i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+    i = i + 1;
+  }
+  list = list + '</ul>';
+
+  return list;
+}
 
 var app = http.createServer(function(request, response) {
   var _url = request.url;
@@ -8,6 +39,7 @@ var app = http.createServer(function(request, response) {
   var pathname = url.parse(_url, true).pathname;
 
   // console.log(queryData.id);
+  // console.log(pathname)
 
   // if (_url == '/') {
   //   // url = '/index.html';
@@ -23,7 +55,6 @@ var app = http.createServer(function(request, response) {
   // console.log(url.parse(_url, true));
   if (pathname === '/') {
     if (queryData.id === undefined) {
-
       fs.readdir('./data', function(error, filelist) {
         // console.log(filelist);
 
@@ -37,78 +68,68 @@ var app = http.createServer(function(request, response) {
         //   <li><a href="/?id=JavaScript">JavaScript</a></li>
         // </ul>`;
 
-        var list = '<ul>';
-        var i = 0;
-        while (i < filelist.length) {
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-          i = i + 1;
-        }
-        list = list + '</ul>';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
 
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
         response.writeHead(200); // 웹서버가 응답할 때
         response.end(template);
       });
-
     } else {
       fs.readdir('./data', function(error, filelist) {
         // console.log(filelist);
 
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-
-        // 참고
-        // var list = `<ul>
-        //   <li><a href="/?id=HTML">HTML</a></li>
-        //   <li><a href="/?id=CSS">CSS</a></li>
-        //   <li><a href="/?id=JavaScript">JavaScript</a></li>
-        // </ul>`;
-
-        var list = '<ul>';
-        var i = 0;
-        while (i < filelist.length) {
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-          i = i + 1;
-        }
-        list = list + '</ul>';
-
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
           var title = queryData.id;
-          var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+
           response.writeHead(200); // 웹서버가 응답할 때
           response.end(template);
         });
       });
     }
-  } else {
+  }
+  else if(pathname === '/create_process') {
+    var body = '';
+
+    request.on('data', function(data) {
+      body = body + data;
+    });
+
+    request.on('end', function() {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      // console.log(post);
+      // console.log(post.title);
+
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+        response.writeHead(302, {Location: `/?id=${title}`}); // 302 리다이렉션
+        response.end();
+      });
+    });
+  }
+  else if(pathname === '/create') {
+    fs.readdir('./data', function(error, filelist) {
+      var title = 'WEB - create';
+      var list = templateList(filelist);
+      var template = templateHTML(title, list, `
+        <form action="http://localhost:3000/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+        </form>
+      `);
+
+      response.writeHead(200); // 웹서버가 응답할 때
+      response.end(template);
+    });
+  }
+  else {
     response.writeHead(404);
     response.end('Not found');
   }
